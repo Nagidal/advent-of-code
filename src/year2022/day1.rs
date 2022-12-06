8f510d7cb509aaea838aafea7e60a4d1128b336c
Sven Siegmund <sven.siegmund@iav.de>
2022-12-06 08:24:59 +0100

    day2 solved

--- src/year2022/day1.rs
+++ src/year2022/day1.rs
@@ -1,24 +1,24 @@
+//use std::any::type_name;
 use std::error::Error;
 
+/// https://stackoverflow.com/a/74298652/923542
+//fn type_of<T>(_: T) -> &'static str {
+//type_name::<T>()
+//}
+
+// thanks raui100 @ github
 pub fn solve() -> Result<(), Box<dyn Error>> {
-    let data_str = include_str!("day1.input.txt");
-    let chunks_str_raw: Vec<&str> = data_str.split("\n\n").collect();
-    let chunks_str: Vec<Vec<&str>> = chunks_str_raw
-        .iter()
-        .map(|s| s.split("\n").collect())
-        .collect();
-    let groups: Vec<Vec<usize>> = chunks_str
-        .iter()
-        .map(|v| {
-            v.iter()
-                .map(|s| s.parse::<usize>().unwrap_or_default())
-                .collect()
+    let max: usize = include_str!("day1.input.txt")
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .lines()
+                .map(|line| line.parse::<usize>().unwrap_or_default())
+                .sum()
         })
-        .collect();
-    let mut sums: Vec<usize> = vec![];
-    for group in &groups {
-        sums.push(group.iter().sum());
-    }
-    println!("{}", sums.iter().max().ok_or("Could not find max value")?);
+        //.reduce(|a: usize, b: usize| a.max(b))
+        .max()
+        .ok_or("Could not find max value")?;
+    println!("{}", max);
     Ok(())
 }
--- src/year2022/day2.rs
+++ src/year2022/day2.rs
@@ -1,6 +1,88 @@
 use std::error::Error;
 
+enum Match {
+    Win,
+    Draw,
+    Loss,
+}
+
+enum RPS {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RPS {
+    fn from_str(s: &str) -> Result<Self, Box<dyn Error>> {
+        match s {
+            "A" => Ok(RPS::Rock),
+            "B" => Ok(RPS::Paper),
+            "C" => Ok(RPS::Scissors),
+            "X" => Ok(RPS::Rock),
+            "Y" => Ok(RPS::Paper),
+            "Z" => Ok(RPS::Scissors),
+            _ => Err("Invalid input".into()),
+        }
+    }
+
+    fn play(&self, other: RPS) -> Match {
+        match self {
+            RPS::Rock => match other {
+                RPS::Rock => Match::Draw,
+                RPS::Paper => Match::Loss,
+                RPS::Scissors => Match::Win,
+            },
+            RPS::Paper => match other {
+                RPS::Rock => Match::Win,
+                RPS::Paper => Match::Draw,
+                RPS::Scissors => Match::Loss,
+            },
+            RPS::Scissors => match other {
+                RPS::Rock => Match::Loss,
+                RPS::Paper => Match::Win,
+                RPS::Scissors => Match::Draw,
+            },
+        }
+    }
+
+    fn score_vs(&self, other: RPS) -> usize {
+        let mut score = 0_usize;
+        match self {
+            RPS::Rock => score += 1,
+            RPS::Paper => score += 2,
+            RPS::Scissors => score += 3,
+        };
+        match self.play(other) {
+            Match::Win => score += 6,
+            Match::Draw => score += 3,
+            Match::Loss => {}
+        }
+        score
+    }
+}
+
 pub fn solve() -> Result<(), Box<dyn Error>> {
-    println!("Not yet solved");
+    let mut results: Vec<usize> = Vec::new();
+    let mut data: Vec<Vec<&str>> = Vec::new();
+    for line in include_str!("day2.input.txt").lines() {
+        data.push(line.split(" ").collect());
+    }
+    for v in data {
+        let mut vi = v.iter();
+        let opponent: RPS = RPS::from_str(vi.next().ok_or_else(|| "")?)?;
+        let me: RPS = RPS::from_str(vi.next().ok_or_else(|| "")?)?;
+        //let b: RPS = RPS::from_str(vi.next().unwrap_or(&""))?;
+        results.push(me.score_vs(opponent));
+    }
+    println!("{:?}", results.iter().sum::<usize>());
+    // now try this functionally:
+    //let result: usize = include_
+    //.lines()
+    //.map(|line| {
+    //line.split(" ")
+    //.chunks(2)
+    //.map(|pair| RPS::from_str(pair.second()?)?.score_vs(RPS::from_str(pair.first()?)?))
+    //})
+    //.sum()?;
     Ok(())
 }
